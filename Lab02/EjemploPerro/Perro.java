f642cd7cada56f1df6e634dafd9107a308a59c3c
Joaquin Fabian Ponze Cateriano <102679415+JoaquinFabian@users.noreply.github.com>
2022-08-23 13:16:37 -0500

    Objetos de java Perro

---
+++ Lab02/EjemploPerro/Perro.java
@@ -0,0 +1,98 @@
+/*Autor: Joaquin Fabian Ponze Cateriano
+Descripcion: clase perro, atributos, conjunto a sus métodos
+*/
+
+public class Perro {
+
+    private String raza;
+    private String color;
+    private double temp;
+    private int edad;
+    public double peso;
+
+    public Perro(String raza, String color) {
+        this.raza = raza;
+        this.color = color;
+        this.temp = 37;
+        this.edad = 1;
+        this.peso = 0.5;
+    }
+
+    public Perro() {
+        this.raza = "chusco";
+        this.color = "gris";
+        this.temp = 37;
+        this.edad = 1;
+        this.peso = 0.5;
+    }
+
+    /*-----------------Gets - Sets ----------------------*/
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /*
+     * No se coloca el atributo peso, ya que es publico y get y set
+     * son para atributos de modo private
+     */
+    /*-----------------Gets - Sets ----------------------*/
+
+    /*---------------------Métodos----------------------*/
+
+    public void ladrar() {
+        System.out.println("Guau Guau");
+    }
+
+    public void dormir() {
+        System.out.println("zzz zzzz");
+    }
+
+    public void comer() {
+        System.out.println("pka pka");
+    }
+
+    public void increEdad(int anios) {
+        this.edad = this.edad + anios;
+        cambiarPeso(anios);
+    }
+
+    private void cambiarPeso(int anios) {
+        this.peso = this.peso + (anios * 1.5);
+    }
+
+    /*---------------------Métodos----------------------*/
+    /* Método especial para llamar a todos los atributos del objeto y mostrarlo */
+    public String tostring() {
+        return "Perro(" + "raza=" + raza + ", color=" + color + ", temp=" + temp +
+        ", edad=" + edad + ", peso=" + peso + ')';
+    }
+}
---
+++ Lab02/EjemploPerro/PrincipalPerro.java
@@ -0,0 +1,11 @@
+public class PrincipalPerro {
+    public static void main(String[] args) {
+
+        Perro Fido = new Perro();
+        Perro Pelusa = new Perro("Pastor Aleman", "Amarillo y Negro");
+
+        System.out.println(Fido.tostring());
+        System.out.println(Pelusa.tostring());
+
+    }
+}
