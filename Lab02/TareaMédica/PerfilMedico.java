1a29ca04c7b99efe66065ee419d9209c6e94554e
Joaquin Fabian Ponze Cateriano <102679415+JoaquinFabian@users.noreply.github.com>
2022-08-25 08:12:07 -0500

    Tarea lab02

---
+++ Lab02/TareaMédica/PerfilMedico.java
@@ -0,0 +1,31 @@
+package Lab02.TareaMédica;
+
+public class PerfilMedico {
+
+
+    /*-----------------Variables----------------------*/
+
+    String primernombre;
+    String apellido;
+    String genero;
+    int dia;
+    int mes;
+    int anio;
+    double altura;
+    double peso;
+
+    /*-----------------Constructores----------------------*/
+
+    public PerfilMedico() {
+        this.primernombre = "";
+        this.apellido = "";
+        this.genero = "";
+        this.dia = 0;
+        this.mes = 0;
+        this.anio= 0;
+        this.altura = 0.0;
+        this.peso = 0.0;
+    }
+
+
+}
